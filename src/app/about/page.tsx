ae7210979d10e59feb49cf0c8890d843f1869035
Florante Clavano <fgclavano@gmail.com>
2024-09-21 00:07:03 +0800

    Layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function page() {
+  return (
+    <main className="p-4 space-y-4">
+      <div className="text-4xl font-bold">About Us</div>
+      <div className="text-lg">About page</div>
+    </main>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Link from "next/link";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -26,9 +27,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased px-2 md:px-5`}
       >
-        {children}
+        <header className="text-white font-bold bg-green-900 p-4 text-2xl">
+          <div className="flex flex-grow">
+            <Link href="/">GPT Chat</Link>
+            <Link href="/about" className="ml-5 font-light">
+              About
+            </Link>
+          </div>
+        </header>
+        <div className="flex flex-col md:flex-grow">
+          <div className="flex-grow"> {children}</div>
+        </div>
       </body>
     </html>
   );
