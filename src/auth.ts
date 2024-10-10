fa7b547d6e30689aa00e0ed88805c0aac380e20e
Florante Clavano <fgclavano@gmail.com>
2024-10-10 12:04:02 +0800

    Made everyone sigin on github

--- src/auth.ts
+++ src/auth.ts
@@ -2,12 +2,6 @@ import NextAuth, { NextAuthConfig } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
 const authOptions: NextAuthConfig = {
-  callbacks: {
-    async signIn({ profile }) {
-      // Change this to your username
-      return profile?.login === "thatkidplongy";
-    },
-  },
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID ?? "",
@@ -15,7 +9,6 @@ const authOptions: NextAuthConfig = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  // basePath: "/api/auth",
 };
 
 export const { handlers, auth, signIn, signOut } = NextAuth(authOptions);
